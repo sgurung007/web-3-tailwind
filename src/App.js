433016310a57dc9bdabc453532d70892b7401103
sgrg007 <sgrg007@gmail.com>
2022-04-29 21:27:24 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <div className="container mx-auto bg-indigo-500 mx-auto">
+                <h3>This is header 3</h3>
+                <p className={"bg-red-100 font-mono text-base"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
+                    aperiam
+                    consequuntur dolorum magni minima porro quos repellendus rerum velit! Architecto dicta facere itaque
+                    libero nostrum pariatur reiciendis soluta ullam, veniam.</p>
+            </div>
+        </>
+    );
 }
 
 export default App;
